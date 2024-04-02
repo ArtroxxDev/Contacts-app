591de3f143815d2272602cfa48f590224fdb541b
Matias López <alopez@eglobalt.com.py>
2024-04-02 15:48:39 -0400

    a

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
-<?php require "partials/header.php" ?>
+<?php
+session_start();
+require "partials/header.php"
+
+?>
+
 
 <div class="welcome d-flex align-items-center justify-content-center">
   <div class="text-center">
     <h1>Store Your Contacts Now</h1>
-    <a class="btn btn-lg btn-dark" href="register.php">Get Started</a>
+
+    <?php if (isset($_SESSION["user"])) : ?>
+      <a class="btn btn-lg btn-dark" href="add.php">Add One!</a>
+    <?php else : ?>
+      <a class="btn btn-lg btn-dark" href="register.php">Get Started!</a>
+    <?php endif ?>
   </div>
 </div>
 
--- login.php
+++ login.php
@@ -36,7 +36,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 // $_SESSION["user_id"] = $user["id"];
                 // $_SESSION["user_name"] = $user["name"];
                 // $_SESSION["user_email"] = $user["email"];
-                header("Location: index.php");
+                header("Location: home.php");
             }
 
         }
